5d7c4976140d6c43be8516fa0a60bda27caecfa4
schang1146 <sammychang2185@gmail.com>
2019-08-14 22:41:04 -0700

    finished knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+    development: {
+        client: "sqlite3",
+        useNullAsDefault: true, // needed for sqlite3
+        connection: {
+            filename: "./data/recipes.db3"
+        },
+        migrations: {
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
+        }
+    }
+};
